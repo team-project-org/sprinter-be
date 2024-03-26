ac66543ed86cdb9eaf6bfb1f658392b1ffe694c2
choigy1001 <easytwo10@naver.com>
2024-03-27 00:13:58 +0900

    링크드인 랜더링 구현

---
+++ src/main/kotlin/hackathon/sprinter/linkedin/resolver/LinkedInController.kt
@@ -0,0 +1,17 @@
+package hackathon.sprinter.linkedin.resolver
+
+import hackathon.sprinter.linkedin.service.LinkedInQueryService
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
+@Controller
+class LinkedInController(
+    private val linkedInQueryService: LinkedInQueryService
+) {
+    @GetMapping
+    fun getLinkedInHTML(@RequestParam url: String): String {
+        return linkedInQueryService.getLinkedInHTML(url)
+    }
+
+}
---
+++ src/main/kotlin/hackathon/sprinter/linkedin/service/LinkedInQueryService.kt
@@ -0,0 +1,56 @@
+package hackathon.sprinter.linkedin.service
+
+import hackathon.sprinter.util.WebDriverUtil
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Service
+import java.time.Duration
+
+
+@Service
+class LinkedInQueryService(
+    private val webDriverUtil: WebDriverUtil,
+    private val redisTemplate: RedisTemplate<String, String>
+) {
+    companion object {
+        private const val LINKEDIN_LOGIN_URL =
+            "https://www.linkedin.com/login/ko?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin"
+    }
+
+    @Value("\${linkedin.username}")
+    private lateinit var username : String
+
+    @Value("\${linkedin.password}")
+    private lateinit var password: String
+
+    fun getLinkedInHTML(linkedInUrl: String): String {
+        val webDriver = webDriverUtil.getWebDriver()
+
+        val opsForValue = redisTemplate.opsForValue()
+        opsForValue.get(linkedInUrl)
+            ?.let { return it }
+
+        loginLinkedIn(webDriver)
+        webDriver.get(linkedInUrl)
+
+        webDriver.pageSource
+            .let {
+                opsForValue.set(linkedInUrl, it, Duration.ofDays(1))
+                return it
+            }
+    }
+
+    private fun loginLinkedIn(webDriver: WebDriver) {
+        webDriver.get(LINKEDIN_LOGIN_URL);
+
+        webDriver.findElement(By.id("username"))
+            .sendKeys(username)
+        webDriver.findElement(By.id("password"))
+            .sendKeys(password)
+        val loginButton =
+            webDriver.findElement(By.cssSelector("#organic-div > form > div.login__form_action_container > button"))
+        loginButton.click()
+    }
+}
