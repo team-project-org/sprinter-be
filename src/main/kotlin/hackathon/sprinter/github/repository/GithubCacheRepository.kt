8e675d5352a3df18b3c4f3170ffd9f8075e169dd
choigy1001 <easytwo10@naver.com>
2024-03-27 00:08:20 +0900

    사용하지 않는 파일 삭제

--- src/main/kotlin/hackathon/sprinter/github/repository/GithubCacheRepository.kt
+++
@@ -1,8 +0,0 @@
-package hackathon.sprinter.github.repository
-
-import hackathon.sprinter.github.model.GithubCache
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface GithubCacheRepository : JpaRepository<GithubCache, Long> {
-    fun findByUsername(username: String) : GithubCache?
-}
--- src/main/kotlin/hackathon/sprinter/linkedin/repository/LinkedInCacheRepository.kt
+++
@@ -1,8 +0,0 @@
-package hackathon.sprinter.linkedin.repository
-
-import hackathon.sprinter.linkedin.model.LinkedInCache
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface LinkedInCacheRepository : JpaRepository<LinkedInCache, Long> {
-    fun findLinkedInCacheByProfileUrl(profileUrl: String) : LinkedInCache?
-}
