9303b455a99b26de1e3147d6765dc4febe33320c
choigy1001 <easytwo10@naver.com>
2024-03-27 00:19:22 +0900

    사용하지 않는 파일, 코드 삭제

--- src/main/kotlin/hackathon/sprinter/github/dto/GithubHTML.kt
+++
@@ -1,6 +0,0 @@
-package hackathon.sprinter.github.dto
-
-data class GithubHTML(
-    val githubHTML: Any
-)
-
--- src/main/kotlin/hackathon/sprinter/util/JSoupUtil.kt
+++ src/main/kotlin/hackathon/sprinter/util/JSoupUtil.kt
@@ -1,14 +1,8 @@
 package hackathon.sprinter.util
 
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 
 fun getHTMLFromGithubUrl(url: String): String {
-
     val document = Jsoup.connect(url).timeout(5000).get()
     return document.toString()
-//    val parse = Jsoup.parse(toString)
-
-//    return document
-//    return document.getElementsByClass("application-main").toString()
 }
