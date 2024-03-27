68200aa8b50ea9a60d8b8432c83d446b9dbe2938
choigy1001 <easytwo10@naver.com>
2024-03-27 23:17:32 +0900

    링크드인 컨트롤러 수정, 크롬 드라이버 업로드

--- src/main/kotlin/hackathon/sprinter/linkedin/resolver/LinkedInController.kt
+++ src/main/kotlin/hackathon/sprinter/linkedin/resolver/LinkedInController.kt
@@ -1,16 +1,16 @@
 package hackathon.sprinter.linkedin.resolver
 
 import hackathon.sprinter.linkedin.service.LinkedInQueryService
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class LinkedInController(
     private val linkedInQueryService: LinkedInQueryService
 ) {
     @GetMapping("/linkedIn")
-    fun getLinkedInHTML(@RequestParam url: String): String {
+    fun getLinkedInHTML(@RequestParam url: String): Any {
         return linkedInQueryService.getLinkedInHTML(url)
     }
 
